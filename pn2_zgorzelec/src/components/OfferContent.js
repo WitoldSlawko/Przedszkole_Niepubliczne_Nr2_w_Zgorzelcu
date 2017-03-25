6dd6c90b12546d187b3d7b6eabc9a10beaf52fd7
WitoldSlawko <witold.slawko@gmail.com>
2017-03-25 23:03:10 +0100

    content of offer component added

--- pn2_zgorzelec/src/components/OfferContent.js
+++ pn2_zgorzelec/src/components/OfferContent.js
@@ -1,25 +1,43 @@
 import React from 'react';
 
-    let styl = {
-      position:'relative',
-      height:'0',
-      paddingBottom:'56.21%'
-    }
-    
-    let styll = {
-      position:'absolute',
-      width:'100%',
-      height:'100%',
-      left:'0',
-      width:'641px', 
-      height:'360px'
-    }
 
 class OfferContent extends React.Component {
 render() {
     return (
-        <div style={styl}>
-          <iframe src="https://www.youtube.com/embed/X7kZVH849Eo?ecver=2" style={styll}></iframe>
+        <div className="oferta">
+          <p>
+            <br/>
+            Dzieci w przedszkolu w radosnej atmosferze nawiązują kontakty i przyjaźnie. Wpływa to korzystnie na rozwój zachowań społecznych takich jak: tolerancja, ustępowanie, pomoc, cierpliwość, odwaga.
+            <br/><br/> 
+            NASZE PRZEDSZKOLE OFERUJE: 
+            <br/>
+            * kontakty ze środowiskiem
+            <br/>
+            * poznawanie przyrody 
+            <br/>
+            * spotkania z ciekawymi ludźmi 
+            <br/>
+            * imprezy kulturalne: kino, teatr,
+            warsztaty teatralne 
+            <br/>
+            * comiesięczne koncerty muzyków z Filharmonii Jeleniogórskiej na miejscu w przedszkolu
+            <br/>
+            * Bal Przebierańców, Dzień Dziecka, Święto Babci i Dziadka 
+            <br/>
+            * bezpłatnie rytmikę, warsztaty plastyczne, zabawy ruchowe 
+            <br/>
+            * dodatkowa oferta za opłata to j. niemiecki, j. angielski.
+            <br/><br/> 
+            Jesteśmy jedynym przedszkolem na terenie Zgorzelca, które od 14 lat systematycznie współpracuje z przedszkolem Zwergenhaus w Goerlitz. W ramach tej współpracy oferujemy dla dzieci wycieczki do Teatru w Goerliz, Muzeum Przyrodniczego, Pracowni Ceramiki, ZOO, wycieczki autokarowe do parku Dinozaurów w Bautzen (wymagany jest paszport dziecka). 
+            <br/><br/>
+            Czynnie uczestniczymy w imprezach na terenie miasta. 
+            <br/><br/>
+            Przedszkole czynne jest w godz. 6:30-16:30. 
+            <br/><br/>
+            Przyjmujemy dzieci do grup rozwojowych: trzy i czterolatki, pięciolatki, sześciolatki. Dzieci znajdują się pod opieką wykwalifikowanej kadry pedagogicznej. Jesteśmy otwarci na propozycje rodziców. 
+            <br/><br/>
+            ZAPRASZAMY DO NASZEGO PRZEDSZKOLA! 
+          </p>
         </div>
     );
 }
