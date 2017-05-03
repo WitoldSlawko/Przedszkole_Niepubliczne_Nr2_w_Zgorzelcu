17cf8264d04cd1f4e7d8e4144c61b0380b9a0ee1
WitoldSlawko <witold.slawko@gmail.com>
2017-05-03 11:26:38 +0200

    files corrected

--- pn2_zgorzelec/src/index.js
+++ pn2_zgorzelec/src/index.js
@@ -14,69 +14,7 @@ import Gallery_3 from './components/Gallery_3';
 import Gallery_4 from './components/Gallery_4';
 import Gallery_5 from './components/Gallery_5';
 import Gallery_6 from './components/Gallery_6';
-//import Main from './components/Main';
-/*
-class App extends React.Component {
-  constructor(props) {
-    super();
-    this.state = {
-      clicked1: false,
-      clicked2: false,
-      clicked3: false,
-      clicked4: false
-    };
-    this.handleClick1 = this.handleClick1.bind(this);
-    this.handleClick2 = this.handleClick2.bind(this);
-    this.handleClick3 = this.handleClick3.bind(this);
-    this.handleClick4 = this.handleClick4.bind(this);
-    }
-    handleClick1() {
-      this.setState({
-        clicked1: true,
-        clicked2: false,
-        clicked3: false,
-        clicked4: false
-      });
-    }
-    handleClick2() {
-      this.setState({
-        clicked1: false,
-        clicked2: true,
-        clicked3: false,
-        clicked4: false
-      });
-    }
-    handleClick3() {
-      this.setState({
-        clicked1: false,
-        clicked2: false,
-        clicked3: true,
-        clicked4: false
-      });
-    }
-    handleClick4() {
-     this.setState({
-        clicked1: false,
-        clicked2: false,
-        clicked3: false,
-        clicked4: true
-     });
-   }
-   render() {
-    return (
-     <Main textToRender={this.props.children} 
-			 		click1={this.state.clicked1}
-			 		click2={this.state.clicked2}
-			 		click3={this.state.clicked3} 
-			 		click4={this.state.clicked4}
-			 		handleClick1={this.handleClick1}
-			 		handleClick2={this.handleClick2}
-			 		handleClick3={this.handleClick3}
-			 		handleClick4={this.handleClick4}/>
-    );
-   }
-};
-*/
+
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
