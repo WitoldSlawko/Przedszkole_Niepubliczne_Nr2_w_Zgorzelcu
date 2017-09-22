d744dc909843de63a8512c7be6fb04a077c88e4f
WitoldSlawko <witold.slawko@gmail.com>
2017-09-22 22:25:49 +0200

    bgc changed

--- pn2_zgorzelec/src/components/Main.js
+++ pn2_zgorzelec/src/components/Main.js
@@ -13,19 +13,19 @@ class Main extends Component {
         {this.props.click1 ? 
 					<Link onClick={this.props.handleClick1} style={{ background: '#1D8787', color: '#FFDBB5', border: '1px solid black' }} to="offercontent">Oferta</Link> 
 					: 
-					<Link onClick={this.props.handleClick1} style={{ background: '#434141', color: '#FFDBB5' }} to="offercontent">Oferta</Link>}
+					<Link onClick={this.props.handleClick1} style={{ background: '#062706', color: '#FFDBB5' }} to="offercontent">Oferta</Link>}
         {this.props.click2 ? 
 					<Link onClick={this.props.handleClick2} style={{ background: '#1D8787', color: '#FFDBB5', border: '1px solid black' }} to="gallerycontent">Galeria</Link> 
 					: 
-					<Link onClick={this.props.handleClick2} style={{ background: '#434141', color: '#FFDBB5' }} to="gallerycontent">Galeria</Link>}
+					<Link onClick={this.props.handleClick2} style={{ background: '#062706', color: '#FFDBB5' }} to="gallerycontent">Galeria</Link>}
         {this.props.click3 ? 
 					<Link onClick={this.props.handleClick3} style={{ background: '#1D8787', color: '#FFDBB5', border: '1px solid black' }} to="moviescontent">Wideoteka</Link> 
 					: 
-					<Link onClick={this.props.handleClick3} style={{ background: '#434141', color: '#FFDBB5' }} to="moviescontent">Wideoteka</Link>}
+					<Link onClick={this.props.handleClick3} style={{ background: '#062706', color: '#FFDBB5' }} to="moviescontent">Wideoteka</Link>}
         {this.props.click4 ? 
 					<Link onClick={this.props.handleClick4} style={{ background: '#1D8787', color: '#FFDBB5', border: '1px solid black' }} to="contactcontent">Kontakt</Link> 
 					: 
-					<Link onClick={this.props.handleClick4} style={{ background: '#434141', color: '#FFDBB5' }} to="contactcontent">Kontakt</Link>}
+					<Link onClick={this.props.handleClick4} style={{ background: '#062706', color: '#FFDBB5' }} to="contactcontent">Kontakt</Link>}
       </nav>
       <main>
         <LeftAside />
